b4e31e1a46068ae9cba1ac8235b4748ae9701088
Gilson <gilson.eniac@gmail>
2025-10-04 09:01:58 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text,StyleSheet} from 'react-native';
+
+
+
+export default function App(){
+  return(
+    <View style={myStyle.conteiner}>
+      <Text>
+        Olá
+      </Text>
+    </View>
+
+
+  );
+}
+const myStyle = StyleSheet.create({
+  conteiner:{
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: '#817a7aff'
+  }
+})
