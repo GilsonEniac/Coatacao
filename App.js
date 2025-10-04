de82be32b79b0eabd916c360ca763bb62564d6ed
Gilson <gilson.eniac@gmail>
2025-10-04 10:20:28 -0300

    criação picker

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
-import React from 'react';
+//import React from 'react';
 import {View, Text,StyleSheet} from 'react-native';
+
+import {PickerItem} from './src/Picker';
 
 
 
 export default function App(){
   return(
-    <View style={myStyle.conteiner}>
-      <Text>
-        Olá
-      </Text>
-    </View>
+   
+   <View style={myStyle.conteiner}>
+      
+      <View style={myStyle.areaMoeda}>
+          <Text style={myStyle.titulo}>Selecione a moeda</Text>
+          <PickerItem/>
+      </View>
+      
+   
+    
 
+   </View>
 
   );
 }
+
 const myStyle = StyleSheet.create({
   conteiner:{
     flex: 1,
-    marginTop: 20,
-    backgroundColor: '#817a7aff'
+    paddingTop:40,
+    alignItems: 'center',
+    backgroundColor: '#101215'
+  },
+  areaMoeda:{
+    backgroundColor: '#f9f9f9',
+    width: '90%',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: 8
+  },
+  titulo:{
+    color: '#000',
+    fontWeight: '500',
+    fontSize: 16,
+    paddingLeft: 5,
+    paddingTop:5
   }
-  
 })
---
+++ src/Picker/index.js
@@ -0,0 +1,17 @@
+
+import {Picker} from '@react-native-picker/picker'
+
+
+
+export function PickerItem(){
+    return(
+
+   <Picker>
+    <Picker.Item value="BTC" key={0} label="BTC"/>
+
+
+   </Picker>
+ 
+    
+    )
+}
